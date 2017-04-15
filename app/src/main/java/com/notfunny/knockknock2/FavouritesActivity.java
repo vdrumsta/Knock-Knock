15a32ccee7d01394c9aa87dac8ca40c93e9c76c3
Vilius Drumsta <vdrumsta@gmail.com>
2017-04-15 09:06:26 +0100

    polished FavouritesActivity

--- app/src/main/java/com/notfunny/knockknock2/FavouritesActivity.java
+++ app/src/main/java/com/notfunny/knockknock2/FavouritesActivity.java
@@ -3,6 +3,8 @@ package com.notfunny.knockknock2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -26,6 +28,8 @@ public class FavouritesActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_favourites);
 
         listView = (ListView) findViewById(R.id.favJokes);
