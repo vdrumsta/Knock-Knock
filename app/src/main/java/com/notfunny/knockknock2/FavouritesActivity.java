9a90c3d5d470e57a43f3c70c1a147658995b0229
Vilius Drumsta <vdrumsta@gmail.com>
2017-04-15 08:30:28 +0100

    added listview to FavouritesActivity

--- app/src/main/java/com/notfunny/knockknock2/FavouritesActivity.java
+++ app/src/main/java/com/notfunny/knockknock2/FavouritesActivity.java
@@ -2,12 +2,53 @@ package com.notfunny.knockknock2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class FavouritesActivity extends AppCompatActivity {
+
+    private ListView listView;
+    private ArrayAdapter<String> adapter;
+    private String[] jokeHeadings = {
+            "Rhino",
+            "Europe",
+            "Anita"
+    };
+    private String[] jokeContent = {
+            "Knock Knock\nWho's there?\nRhino...\nRhino who?\nRhino every knock knock joke",
+            "Knock Knock\nWho's there?\nEurope\nEurope who?\nNo, you're a poo!",
+            "Knock Knock\nWho's there?\nAnita\nAnita who\nAnita suck your dick!"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favourites);
+
+        listView = (ListView) findViewById(R.id.favJokes);
+        adapter = new ArrayAdapter<String>(this, R.layout.listview, R.id.textView1, jokeHeadings);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                TextView textView = (TextView) view.findViewById(R.id.textView2);
+                textView.setText(jokeContent[position]);
+
+                if ( textView.getVisibility() == View.GONE)
+                {
+                    //expandedChildList.set(arg2, true);
+                    textView.setVisibility(View.VISIBLE);
+                }
+                else
+                {
+                    //expandedChildList.set(arg2, false);
+                    textView.setVisibility(View.GONE);
+                }
+            }
+        });
     }
 }
